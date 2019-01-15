4deff5fdabdcc74457759a2a505cade4b6442a8c
Greg Baker <greg.baker@roksostudios.com>
2019-01-15 10:32:09 -0700

    Service correction

--- src/services/GeolocationService.js
+++ src/services/GeolocationService.js
@@ -1,16 +1,35 @@
 // API Key - Change to your API_KEY
-const API_KEY = "MAPQUEST_API_KEY";
+const GEO_API_KEY = "MAPQUEST_API_KEY";
 
 // base URL to MapQuest API
-const BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address';
+const GEO_BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address';
 
 // `GeolocationService` CLASS
 class GeolocationService {
     getCurrentPosition(city, state) {
-        // You can use this function to fetch geographic data
-        // using the MapQuest Geocoding API.
+        if(!city) throw Error("city is required");
+        if(!state) throw Error("state is required");
 
-        // Don't forget promises!
+        // URL to use to acquire the geographic coordinates for a city, state/region
+        // `city` and `state` must be valid string values (e.g. Baltimore,MD)
+        // Don't forget to set your `GEO_API_KEY`
+        const url = `${GEO_BASE_URL}?key=${GEO_API_KEY}&location=${city},${state}`;
+
+        return new Promise((resolve, reject) => {
+            // fetch the data using the above URL
+                // THEN convert response into JSON object
+                // THEN process the `response`
+                    // IF `response.info.statuscode` equals `0`, there is no error, so...
+                        // `response.results` is an array of results
+                            //  - the first one is all you'll need
+                        // `response.results[0].locations` is an array of matching locations
+                            // - the first one is all you'll need
+                        // `response.results[0].locations[0].latLng` is the geographic
+                            // coordinates for the 1st location of the 1st result
+                            // `latLng` is comprised of `lat` and `lng`
+                    // ELSE IF ERROR
+                        // `response.info.messages` is an array of error messages (strings)
+        });
     }
 }
 
