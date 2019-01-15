d9c9459fa62f6cfa859c7864e8b2e781888ab22a
Greg Baker <greg.baker@roksostudios.com>
2019-01-15 10:29:44 -0700

    Services change - give students more

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -10,18 +10,98 @@ const WEATHER_IMG_URL = 'http://openweathermap.org/img/w';
 // `WeatherService` CLASS
 class WeatherService {
     getCurrentWeatherByPosition({latitude, longitude}) {
-        // You can use this function to fetch current weather data
-        // for a geographic coordinate using the OpenWeatherMap API.
+        if(!latitude) throw Error('Latitude is required');
+        if(!longitude) throw Error('Longitude is required');
 
-        // Don't forget promises!
+        const url = `${WEATHER_BASE_URL}/weather?appid=${WEATHER_API_KEY}&lat=${latitude}&lon=${longitude}&units=imperial`;
+        return getWeather(url);
     }
 
     getHourlyWeatherByPosition({latitude, longitude}) {
-        // You can use this function to fetch hourly weather data
-        // for a geographic coordinate using the OpenWeatherMap API.
+        if(!latitude) throw Error('Latitude is required');
+        if(!longitude) throw Error('Longitude is required');
 
-        // Don't forget promises!
+        const url = `${WEATHER_BASE_URL}/forecast?appid=${WEATHER_API_KEY}&lat=${latitude}&lon=${longitude}&units=imperial&cnt=12`;
+        return getHourlyWeather(url);
     }
 }
 
 export default WeatherService;
+
+// `getWeather` FUNCTION
+const getWeather = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if(parseInt(data.cod) === 200) {
+                    const { main, icon } = data.weather[0];
+                    const { temp, temp_min, temp_max } = data.main;
+                    const { lon, lat } = data.coord;
+                    const { dt, name } = data;
+                    resolve({
+                        condition: main,
+                        date: new Date(dt * 1000),
+                        icon: `${WEATHER_IMG_URL}/${icon}.png`,
+                        location: {
+                            name: name,
+                            latitude: lat,
+                            longitude: lon
+                        },
+                        temperature: {
+                            current: temp,
+                            minimum: temp_min,
+                            maximum: temp_max
+                        }
+                    });
+                }
+                else if(data.cod === 429) {
+                    reject('Too many requests to weather API');
+                }
+                else {
+                    reject('Unknown error with weather API');
+                }
+            })
+            .catch(error => reject(error.message));
+    });
+};
+
+// `getHourlyWeather` FUNCTION
+const getHourlyWeather = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // IMPORTANT: This API call returns the status code as a `string`
+                //            Bug reported and confirmed.
+                if(parseInt(data.cod) === 200) {
+                    const location = {
+                        name: data.city.name,
+                        latitude: data.city.coord.lat,
+                        longitude: data.city.coord.lon
+                    };                    
+
+                    const hourlyForecasts = data.list.map(fc => {
+                        return {
+                            condition: fc.weather[0].description,
+                            date: new Date(fc.dt * 1000),
+                            icon: `${WEATHER_IMG_URL}/${fc.weather[0].icon}.png`,
+                            location: location,
+                            temperature: {
+                                current: fc.main.temp
+                            }
+                        };
+                    });
+                    
+                    resolve(hourlyForecasts);
+                }
+                else if(data.cod === 429) {
+                    reject('Too many requests to weather API');
+                }
+                else {
+                    reject('Unknown error with weather API');
+                }
+            })
+            .catch(error => reject(error.message));
+    });
+};
